b5eaae45029070fdc9e78fb374f93334fd0d3b53
gwpjp <john.pearson.02@gmail.com>
2017-10-27 09:18:18 -0400

    Got game working as it's supposed to.

--- logic.js
+++ logic.js
@@ -6,10 +6,10 @@ const dealerHand = new DeckImport.Hand(deck, true);
 
 function initialDeal() {
     console.log('Dealing cards...');
-    playerHand.dealCard();
-    dealerHand.dealCard();
-    playerHand.dealCard();
-    dealerHand.dealCard();
+    for (let i = 0; i < 2; i += 1) {
+        playerHand.dealCard();
+        dealerHand.dealCard();
+    }
     console.log(`Your cards are the ${playerHand.hand[0]} and the ${playerHand.hand[1]}.`);
     console.log(`The dealer has one card face down and the ${dealerHand.hand[1]} showing.`);
 }
@@ -26,25 +26,19 @@ function playPlayer() {
 
 
 function playDealer() {
-    console.log('The dealer makes a decision...');
-    if (dealerHand.sum < 16) {
+    console.log(`The dealer now plays and flips over the hole card, the ${dealerHand.cards[0]}.`);
+    console.log(`The dealer's hand is the ${dealerHand.cards[0]} and the ${dealerHand.cards[1]}.`);
+    console.log('');
+    while (dealerHand.sum < 16) {
         dealerHand.dealCard();
-        console.log(`The dealer drew a card, the ${dealerHand.cards[dealerHand.cards.length - 1]}. `);
-        let seen = `the ${dealerHand.hand[1]}`;
-        for (let i = 2; i < dealerHand.cards.length; i += 1) {
-            seen += `, the ${dealerHand.hand[i]}`;
-        }
-        console.log(`The dealer now has ${dealerHand.cards.length} cards.  They are ${seen}, and the one that is face down.`);
+        console.log(`The dealer draws a card.  It's the ${dealerHand.cards[dealerHand.cards.length - 1]}. `);
         if (dealerHand.sum > 21 && dealerHand.hasAce) {
             dealerHand.aceBig = false;
         }
-    } else {
-        console.log('The dealer stays.');
     }
 }
 
 function decideWinner() {
-    console.log(`The dealer turns over the ${dealerHand.cards[0]}.`);
     if (dealerHand.sum > 21) {
         console.log('The dealer busts.  You win!');
     } else {
@@ -61,3 +55,4 @@ module.exports.initialDeal = initialDeal;
 module.exports.playDealer = playDealer;
 module.exports.playPlayer = playPlayer;
 module.exports.decideWinner = decideWinner;
+module.exports.playerHand = playerHand;
--- play.js
+++ play.js
@@ -3,8 +3,6 @@ const figlet = require('figlet');
 const chalk = require('chalk');
 const logic = require('./logic');
 
-
-
 console.log(chalk.blue(figlet.textSync('Blackjack')));
 console.log('');
 logic.initialDeal();
@@ -20,24 +18,30 @@ const questions = [
 ];
 
 let userHit = 'Hit';
+let noBust = true;
 
 async function run() {
-    while (userHit == "Hit") {
+    while (userHit == "Hit" && noBust) {
         let answer = await inquirer.prompt(questions).then((answers) => {
             console.log('');
             if (answers.user == 'Hit') {
                 logic.playPlayer();
                 console.log('');
-                logic.playDealer();
-                console.log('');
+                if (logic.playerHand.sum > 21) {
+                    console.log('Oh no!  You busted!  You lose.');
+                    noBust = false;
+                }
             } else {
-                logic.decideWinner();
                 userHit = 'Stay';
+                logic.playDealer();
+                setTimeout(function(){
+                    logic.decideWinner();
+                },1000);
             }
         });
     }
+    console.log('');
 }
 
 
 run();
-console.log('');
