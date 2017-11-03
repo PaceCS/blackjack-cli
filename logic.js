a1e5d4ea07cf1ac1020ddfd13ac530a11b03bdc5
gwpjp <john.pearson.02@gmail.com>
2017-11-03 14:02:28 -0400

    Updated skeleton code.

--- logic.js
+++ logic.js
@@ -7,7 +7,7 @@ const dealerHand = new DeckImport.Hand(deck, true);
 // This function will do the initial dealing of the cards.
 //  You should deal 2 cards to each player and then print what each hand is.
 function initialDeal() {
-
+    
 }
 
 // This function will deal the player a new card and print the result.
@@ -27,19 +27,24 @@ function dealPlayer() {
     console.log(`You now have ${print}.`);
 }
 
-// This function will play the dealer.  This is an automated process.
-// The dealer should be dealt a card until the sum of the cards is greater than 16.
-//  If the dealer has an Ace and goes over 21, then that Ace needs to only count as 1 point.
-// You can check whether the dealer has an Ace using dealerHand.hasAce.
-// Then you will need to use the code dealerHand.aceBig = false to lower the point value.
+// This function will play the dealer.
 function playDealer() {
+    // First, tell the user what the dealer's hole card is.
     console.log();
+    // Then, remind the user about the dealer's hand
     console.log();
     console.log('');
-    while () {
 
+    // Now figure out if the dealer needs to draw any cards
+    // The dealer should be dealt a card until the sum of the cards is greater than 16.
+    while () {
+        // Tell the user which card the dealer drew.
         console.log();
+
+        //  If the dealer has an Ace and goes over 21, then that Ace needs to only count as 1 point.
+        // You can check whether the dealer has an Ace using dealerHand.hasAce.
         if () {
+            // Then you will need to use the code dealerHand.aceBig = false to lower the point value.
 
         }
     }
